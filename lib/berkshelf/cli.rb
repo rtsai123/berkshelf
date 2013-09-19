56cd713f7495e2b7734948eac99bbbd0d0d7b6f4
Seth Vargo <sethvargo@gmail.com>
2013-09-19 15:04:36 -0400

    Remove check command

--- lib/berkshelf/cli.rb
+++ lib/berkshelf/cli.rb
@@ -319,24 +319,6 @@ module Berkshelf
       desc: 'Path to a Berksfile to operate off of.',
       aliases: '-b',
       banner: 'PATH'
-    desc 'check', 'Check that all dependencies are downloaded and installed'
-    def check
-      berksfile = Berksfile.from_file(options[:berksfile])
-
-      if berksfile.ok?
-        Berkshelf.ui.info "The Berksfile's dependencies are satisfied and downloaded"
-      else
-        Berkshelf.ui.error "Berkshelf can't satisfy your Berksfile's dependencies"
-        raise CookbookNotFound
-      end
-    end
-
-    method_option :berksfile,
-      type: :string,
-      default: Berkshelf::DEFAULT_FILENAME,
-      desc: 'Path to a Berksfile to operate off of.',
-      aliases: '-b',
-      banner: 'PATH'
     desc 'list', 'List all cookbooks and their dependencies specified by your Berksfile'
     def list
       berksfile = Berksfile.from_file(options[:berksfile])
